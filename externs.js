f3128acf98bfe0f2f39e0f2fd76a99b16df327c8
Anton <anton@adc.sh>
2019-04-25 12:24:06 +0300

    add externs

---
+++ externs.js
@@ -0,0 +1,13 @@
+/* typal types/index.xml */
+/** @const */
+var _fpj = {}
+/**
+ * The options for `fpj`.
+ * @typedef {{ fields: (!Array<string>|undefined), soft: (boolean|undefined) }}
+ */
+_fpj.FPJConfig
+/**
+ * The return type of the program.
+ * @typedef {{ entry: string, packageJson: string, packageName: string, version: (string|undefined), hasMain: (boolean|undefined), entryExists: (boolean|undefined) }}
+ */
+_fpj.FPJReturn
--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import resolveDep from 'resolve-dependency'
  * Finds the location of the `package.json` for the given dependency in the directory, and its entry file.
  * @param {string} dir The path to the directory where the requiring file is located.
  * @param {string} name The name of the required package.
- * @param {FPJConfig} opts The options for `fpj`.
- * @param {Array<string>} [opts.fields] Any additional fields from `package.json` file to return.
+ * @param {!_fpj.FPJConfig} opts The options for `fpj`.
+ * @param {!Array<string>} [opts.fields] Any additional fields from `package.json` file to return.
  * @param {boolean} [opts.soft=false] If the entry export (main or module) does not exist, `soft` mode will not throw an error, but add the `hasEntry` property to the output set to _false_. Default `false`.
- * @returns {Promise<FPJReturn>}
+ * @returns {!Promise<!_fpj.FPJReturn>}
  */
 const findPackageJson = async (dir, name, opts = {}) => {
   const { fields, soft = false } = opts
@@ -18,13 +18,13 @@ const findPackageJson = async (dir, name, opts = {}) => {
   const path = join(fold, 'package.json')
   const e = await exists(path)
   if (e) {
-    const res = await findEntry(path, fields, soft)
+    const res = await findEntry(path, fields)
     if (res === undefined)
       throw new Error(`The package ${relative('', path)} does export the module.`)
     else if (!res.entryExists && !soft)
       throw new Error(`The exported module ${res.main} in package ${name} does not exist.`)
     const { entry, version, packageName, main, entryExists, ...rest } = res
-    return {
+    const result = /** @type {!_fpj.FPJReturn} */ ({
       entry: relative('', entry),
       packageJson: relative('', path),
       ...(version ? { version } : {}),
@@ -32,7 +32,8 @@ const findPackageJson = async (dir, name, opts = {}) => {
       ...(main ? { hasMain: true } : {}),
       ...(!entryExists ? { entryExists: false } : {}),
       ...rest,
-    }
+    })
+    return result
   }
   if (dir == '/' && !e)
     throw new Error(`Package.json for module ${name} not found.`)
@@ -40,7 +41,10 @@ const findPackageJson = async (dir, name, opts = {}) => {
 }
 
 /**
- * Finds the path to the entry based on package.json file. */
+ * Finds the path to the entry based on package.json file.
+ * @param {string} path
+ * @param {!Array<string>} fields
+ */
 export const findEntry = async (path, fields = []) => {
   const f = await read(path)
   let mod, version, packageName, main, rest
@@ -81,11 +85,22 @@ export default findPackageJson
 
 /* documentary types/index.xml */
 /**
- * @typedef {Object} FPJConfig The options for `fpj`.
- * @prop {Array<string>} [fields] Any additional fields from `package.json` file to return.
+ * @suppress {nonStandardJsDocs}
+ * @typedef {_fpj.FPJConfig} FPJConfig The options for `fpj`.
+ */
+/**
+ * @suppress {nonStandardJsDocs}
+ * @typedef {Object} _fpj.FPJConfig The options for `fpj`.
+ * @prop {!Array<string>} [fields] Any additional fields from `package.json` file to return.
  * @prop {boolean} [soft=false] If the entry export (main or module) does not exist, `soft` mode will not throw an error, but add the `hasEntry` property to the output set to _false_. Default `false`.
- *
- * @typedef {Object} FPJReturn The return type of the program.
+ */
+/**
+ * @suppress {nonStandardJsDocs}
+ * @typedef {_fpj.FPJReturn} FPJReturn The return type of the program.
+ */
+/**
+ * @suppress {nonStandardJsDocs}
+ * @typedef {Object} _fpj.FPJReturn The return type of the program.
  * @prop {string} entry The location of the package's entry file. The preference is given to the `module` field.
  * @prop {string} packageJson The path to the package.json file itself.
  * @prop {string} packageName The name of the resolved package.
